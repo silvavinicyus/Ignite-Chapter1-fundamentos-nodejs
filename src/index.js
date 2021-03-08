ff42b4074ead1e2ec2ed27a9f55905fb68398505
Vinicyus <vinicyus346@gmail.com>
2021-03-08 18:11:02 -0300

    Fundamentos do nodejs

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json([
+        "Curso 1", 
+        "Curso 2", 
+        "Curso 3", 
+    ]);
+});
+
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json([
+        "Curso 1", 
+        "Curso 2", 
+        "Curso 3", 
+        "Curso 4", 
+    ]);
+});
+
+app.put("/courses/:id",  (request, response) => {
+    const params = request.params;
+    console.log(params);
+    return response.json([
+        "Curso 6", 
+        "Curso 2", 
+        "Curso 3", 
+        "Curso 4", 
+    ]);
+});
+
+app.patch("/courses/:id",  (request, response) => {
+    return response.json([
+        "Curso 6", 
+        "Curso 7", 
+        "Curso 3", 
+        "Curso 4", 
+    ]);
+});
+
+app.delete("/courses/:id",  (request, response) => {
+    return response.json([
+        "Curso 6", 
+        "Curso 7",         
+        "Curso 4", 
+    ]);
+});
+
+
+app.listen(3333, () => {
+    console.log("Server started at port 3333!");
+});
